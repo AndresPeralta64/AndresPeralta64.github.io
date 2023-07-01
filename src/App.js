abbd40e556fe473b363dcb5df21b8a41fd9a0186
AndresPeralta64 <138265624+AndresPeralta64@users.noreply.github.com>
2023-06-30 20:39:02 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div class="e51_22">
+        <div  class="e51_23"></div>
+          <span  class="e51_24">Noticias y descubrimientos astronómicos</span>
+        <div  class="e51_25"></div>
+          <span  class="e51_26">Se ha descubierto agua en la Luna. Este descubrimiento sugiere la existencia de depósitos de agua helada en áreas sombreadas de la Luna, lo que podría ser valioso para futuras misiones espaciales y la exploración del sistema solar.</span>
+        <div  class="e51_27"></div>
+        <div  class="e51_28"></div>
+          <span  class="e51_29">La exploración de los satélites de Júpiter ha sido fascinante en la exploración espacial. Los satélites jovianos son objetos de interés debido a su diversidad geológica, atmósferas únicas y la posibilidad de albergar vida.</span>
+        <div  class="e51_30"></div>
+        <div  class="e51_31"></div>
+        <div  class="e51_32"></div>
+        <div  class="e51_33"></div>
+        <div  class="e51_34"></div>
+        <div  class="e51_35"></div>
+          <span  class="e51_53">Hecho por Andres Peralta</span>
+        <div class="e51_36">
+          <div  class="e51_37"></div>
+          <span  class="e51_38">Descubre más</span>
+        </div>
+        <div  class="e51_39"></div>
+          <span  class="e51_40">DESCUBRIMIENTO DE AGUA EN LA LUNA</span>
+        <div  class="e51_41"></div>
+        <div  class="e51_42"></div>
+        <div  class="e51_43"></div>
+          <span  class="e51_44">EXPLORACIÓN DE SATÉLITAS DE JUPITER</span>
+          <span  class="e51_45">EXPLORACIÓN DE SATÉLITAS DE JUPITER</span>
+          <span  class="e51_46">HUBBLE DESCUBRE UNA ESTRELLA LEJANA</span>
+          <span  class="e51_47">DESCUBRIMIENTO DE ONDAS GRAVITACIONALES</span>
+          <span  class="e51_48">EXPLORACIÓN EN MARTE</span>
+          <span  class="e51_49">La exploración de Marte ha sido un objetivo de varios países durante décadas, aunque han fracasado. Estados Unidos ha logrado aterrizar en Marte en nueve ocasiones, mientras que la Unión Soviética y China han aterrizado una nave espacial.</span>
+          <span  class="e51_50">El descubrimiento de las ondas gravitacionales confirmó la existencia de perturbaciones en el tejido del espacio-tiempo, predichas por Einstein, abriendo una nueva forma de observar y estudiar el universo.</span>
+          <span  class="e51_51">El telescopio Hubble capturó la imagen de la estrella más lejana jamás vista. Este descubrimiento amplía nuestro conocimiento sobre la formación y evolución de las estrellas, así como sobre la expansión del universo en sus etapas tempranas.</span>
+          <span  class="e51_52">La sonda Parker Solar Probe de la NASA explora el Sol de cerca, recopilando datos innovadores sobre la corona y el viento solar. Su investigación ayuda a entender mejor el Sol y su influencia en el sistema solar.</span>
+      </div>
+    </body>
+  );
+}
+
+export default App;
